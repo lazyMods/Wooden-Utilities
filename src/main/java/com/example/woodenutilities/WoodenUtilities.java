61054d08fd46f30a68742fb15515e23fa0efa349
lazynessmind <lazynessmind@gmail.com>
2021-07-25 23:44:48 +0100

    Uncommented the mod initialization.

--- src/main/java/com/example/woodenutilities/WoodenUtilities.java
+++ src/main/java/com/example/woodenutilities/WoodenUtilities.java
@@ -3,10 +3,6 @@ package com.example.woodenutilities;
 import com.example.woodenutilities.common.config.ModConfig;
 import com.example.woodenutilities.common.init.ModInit;
 import com.example.woodenutilities.common.utility.ModConstants;
-import com.example.woodenutilities.common.utility.ModUtils;
-//import me.shedaniel.autoconfig.AutoConfig;
-//import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -16,9 +12,9 @@ public class WoodenUtilities {
     public static ModConfig config;
 
     public WoodenUtilities() {
+        ModInit.init(FMLJavaModLoadingContext.get().getModEventBus());
         // TODO re-add config
 //        AutoConfig.register(ModConfig.class, Toml4jConfigSerializer::new);
-//        ModInit.init(FMLJavaModLoadingContext.get().getModEventBus());
 //        config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
 //        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, ModUtils::createConfigScreen);
     }
