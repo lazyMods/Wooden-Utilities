afa1f8eb1a6e93163ad675a5d87d87fe991db40b
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-07-27 23:47:06 +0100

    Cleanup and method to create basic items.

--- src/main/java/com/example/woodenutilities/common/utility/ModUtils.java
+++ src/main/java/com/example/woodenutilities/common/utility/ModUtils.java
@@ -1,17 +1,13 @@
 package com.example.woodenutilities.common.utility;
 
-//import com.example.woodenutilities.common.config.ModConfig;
-//import me.shedaniel.autoconfig.AutoConfig;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.world.item.Item;
+import com.example.woodenutilities.common.init.ModInit;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 import javax.annotation.Nonnull;
-import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
 public class ModUtils {
@@ -27,7 +23,11 @@ public class ModUtils {
         };
     }
 
-//    public static BiFunction<Minecraft, Screen, Screen> createConfigScreen() {
-//        return (client, parent) -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
-//    }
+    public static Item createBasicItem() {
+        return new Item(new Item.Properties().tab(ModInit.CREATIVE_TAB));
+    }
+
+    /*public static BiFunction<Minecraft, Screen, Screen> createConfigScreen() {
+        return (client, parent) -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+    }*/
 }
