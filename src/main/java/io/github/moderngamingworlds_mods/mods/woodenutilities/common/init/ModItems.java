1648cfb0b332197a24525f1e42ac324cbac7466f
MelanX <info@melanx.de>
2021-09-25 18:50:05 +0200

    fix wooden shears being consumed

--- src/main/java/io/github/moderngamingworlds_mods/mods/woodenutilities/common/init/ModItems.java
+++ src/main/java/io/github/moderngamingworlds_mods/mods/woodenutilities/common/init/ModItems.java
@@ -1,10 +1,9 @@
 package io.github.moderngamingworlds_mods.mods.woodenutilities.common.init;
 
-import io.github.moderngamingworlds_mods.mods.woodenutilities.WoodenUtilities;
 import io.github.moderngamingworlds_mods.mods.woodenutilities.common.item.BaseItem;
+import io.github.moderngamingworlds_mods.mods.woodenutilities.common.item.WoodenShears;
 import io.github.moderngamingworlds_mods.mods.woodenutilities.common.item.buckets.WoodenBucketItem;
 import io.github.noeppi_noeppi.libx.annotation.registration.RegisterClass;
-import io.github.noeppi_noeppi.libx.base.ItemBase;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.material.Fluids;
 
@@ -20,7 +19,7 @@ public class ModItems {
     public static final Item crimsonPlate = new BaseItem();
     public static final Item warpedPlate = new BaseItem();
 
-    public static final Item woodenShears = new ItemBase(WoodenUtilities.getInstance(), new Item.Properties().durability(119));
+    public static final Item woodenShears = new WoodenShears();
     public static final Item woodenDiamond = new BaseItem();
 
     public static final Item woodenBucket = new WoodenBucketItem(() -> Fluids.EMPTY);
---
+++ src/main/java/io/github/moderngamingworlds_mods/mods/woodenutilities/common/item/WoodenShears.java
@@ -0,0 +1,29 @@
+package io.github.moderngamingworlds_mods.mods.woodenutilities.common.item;
+
+import io.github.moderngamingworlds_mods.mods.woodenutilities.WoodenUtilities;
+import io.github.noeppi_noeppi.libx.base.ItemBase;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class WoodenShears extends ItemBase {
+
+    public WoodenShears() {
+        super(WoodenUtilities.getInstance(), new Item.Properties().durability(119));
+    }
+
+    @Override
+    public boolean hasContainerItem(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getContainerItem(ItemStack stack) {
+        int damage = stack.getDamageValue() + 1;
+        if (damage >= stack.getMaxDamage()) {
+            return ItemStack.EMPTY;
+        }
+
+        stack.setDamageValue(damage);
+        return stack.copy();
+    }
+}
