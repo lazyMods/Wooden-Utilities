c4a569b2610c9db302336e17a451ded0f802cc15
MelanX <info@melanx.de>
2021-09-25 18:10:13 +0200

    remove lang generator

--- src/main/java/io/github/moderngamingworlds_mods/mods/woodenutilities/common/config/ModConfig.java
+++ src/main/java/io/github/moderngamingworlds_mods/mods/woodenutilities/common/config/ModConfig.java
@@ -10,13 +10,13 @@ public class ModConfig {
     @Group
     public static class WoodenBucket {
 
-        @Config
+        @Config("The time in ticks that the Wooden Bucket will take to self burn when picking a hot fluid.")
         public static int destroyTime = 50;
 
-        @Config
+        @Config("The max temperature that the Wooden Bucket can hold until burns itself.")
         public static int maxTemperature = 1300;
 
-        @Config
+        @Config("Number of seconds that the player will be set on fire when the bucket burn itself.")
         public static int fireTime = 5;
     }
 }
