9682b6f418adcd06dbe269101a2cdaf435310d2c
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-11-25 18:49:04 +0000

    Add mod id global variable.
    This prevents misspelling the mod id when writing it manually.

--- src/main/java/io/github/moderngamingworlds_mods/woodenutilities/WoodenUtilities.java
+++ src/main/java/io/github/moderngamingworlds_mods/woodenutilities/WoodenUtilities.java
@@ -1,23 +1,34 @@
 package io.github.moderngamingworlds_mods.woodenutilities;
 
 import io.github.moderngamingworlds_mods.woodenutilities.common.init.ModItems;
+import io.github.moderngamingworlds_mods.woodenutilities.common.tests.BucketTests;
 import io.github.noeppi_noeppi.libx.mod.registration.ModXRegistration;
 import io.github.noeppi_noeppi.libx.mod.registration.RegistrationBuilder;
+import net.minecraft.gametest.framework.GameTestRegistry;
+import net.minecraft.gametest.framework.TestCommand;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLanguageProvider;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmlserverevents.FMLServerStartedEvent;
+import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 
 import javax.annotation.Nonnull;
 
-@Mod("woodenutilities")
+@Mod(WoodenUtilities.MOD_ID)
 public final class WoodenUtilities extends ModXRegistration {
+
+    public static final String MOD_ID = "woodenutilities";
 
     private static WoodenUtilities instance;
 
     public WoodenUtilities() {
-        super(new CreativeModeTab("woodenutilities") {
+        super(new CreativeModeTab(MOD_ID) {
             @Nonnull
             @Override
             public ItemStack makeIcon() {
@@ -29,7 +40,6 @@ public final class WoodenUtilities extends ModXRegistration {
 
     @Override
     protected void setup(FMLCommonSetupEvent event) {
-
     }
 
     @Override
