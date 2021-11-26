2b63baddd30d4190aff8f1e704b627b3bfd47636
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-11-26 21:55:17 +0000

    Removed static MOD_ID

--- src/main/java/io/github/moderngamingworlds_mods/woodenutilities/WoodenUtilities.java
+++ src/main/java/io/github/moderngamingworlds_mods/woodenutilities/WoodenUtilities.java
@@ -1,34 +1,23 @@
 package io.github.moderngamingworlds_mods.woodenutilities;
 
 import io.github.moderngamingworlds_mods.woodenutilities.common.init.ModItems;
-import io.github.moderngamingworlds_mods.woodenutilities.common.tests.BucketTests;
 import io.github.noeppi_noeppi.libx.mod.registration.ModXRegistration;
 import io.github.noeppi_noeppi.libx.mod.registration.RegistrationBuilder;
-import net.minecraft.gametest.framework.GameTestRegistry;
-import net.minecraft.gametest.framework.TestCommand;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLanguageProvider;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmlserverevents.FMLServerStartedEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 
 import javax.annotation.Nonnull;
 
-@Mod(WoodenUtilities.MOD_ID)
+@Mod("woodenutilities")
 public final class WoodenUtilities extends ModXRegistration {
-
-    public static final String MOD_ID = "woodenutilities";
 
     private static WoodenUtilities instance;
 
     public WoodenUtilities() {
-        super(new CreativeModeTab(MOD_ID) {
+        super(new CreativeModeTab("woodenutilities") {
             @Nonnull
             @Override
             public ItemStack makeIcon() {
